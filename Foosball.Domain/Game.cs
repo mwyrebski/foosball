b9f681c62416e22c2dfbb4da6fd6800583482264
Michal Wyrebski <michal.wyrebski@gmail.com>
2018-07-01 11:49:45 +0200

    Green. Create game with empty sets

--- Foosball.Domain/Game.cs
+++ Foosball.Domain/Game.cs
@@ -6,9 +6,15 @@ namespace Foosball.Domain
 {
     public class Game
     {
+        public IList<Set> Sets { get; set; } = new List<Set>();
+
         public static Game Create()
         {
             return new Game();
         }
+    }
+
+    public class Set
+    {
     }
 }
