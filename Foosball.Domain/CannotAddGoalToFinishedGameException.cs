b7184103573acbfe006ac74f2d0f25e4707799b1
Michal Wyrebski <michal.wyrebski@gmail.com>
2018-07-01 13:31:33 +0200

    Add exception class: CannotAddGoalToFinishedGameException

---
+++ Foosball.Domain/CannotAddGoalToFinishedGameException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Foosball.Domain
+{
+    public class CannotAddGoalToFinishedGameException : Exception
+    {
+        public CannotAddGoalToFinishedGameException()
+            : base("Cannot add goal to a Finished game")
+        {
+        }
+    }
+}
