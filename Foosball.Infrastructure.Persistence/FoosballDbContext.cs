c5b35439498515161b89bbb0bcfca8b43c7957c6
Michal Wyrebski <michal.wyrebski@gmail.com>
2018-07-01 17:20:09 +0200

    Add Initial migration for Sqlite

--- Foosball.Infrastructure.Persistence/FoosballDbContext.cs
+++ Foosball.Infrastructure.Persistence/FoosballDbContext.cs
@@ -5,8 +5,17 @@ namespace Foosball.Infrastructure.Persistence
 {
     public class FoosballDbContext : DbContext
     {
+        public FoosballDbContext()
+        {
+        }
+
         public FoosballDbContext(DbContextOptions options) : base(options)
         {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Foosball.configure.db");
         }
 
         public DbSet<Game> Games { get; set; }
---
+++ Foosball.Infrastructure.Persistence/Migrations/20180701151925_InitialCreate.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Foosball.Infrastructure.Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StartedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.GameId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sets",
+                columns: table => new
+                {
+                    SetId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    GameId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sets", x => x.SetId);
+                    table.ForeignKey(
+                        name: "FK_Sets_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "GameId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Goals",
+                columns: table => new
+                {
+                    GoalId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    Team = table.Column<int>(nullable: false),
+                    SetId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Goals", x => x.GoalId);
+                    table.ForeignKey(
+                        name: "FK_Goals_Sets_SetId",
+                        column: x => x.SetId,
+                        principalTable: "Sets",
+                        principalColumn: "SetId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Goals_SetId",
+                table: "Goals",
+                column: "SetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sets_GameId",
+                table: "Sets",
+                column: "GameId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Goals");
+
+            migrationBuilder.DropTable(
+                name: "Sets");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
