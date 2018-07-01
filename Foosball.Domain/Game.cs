5ff3d2a50863d2f9e125327fed72628ce7171647
Michal Wyrebski <michal.wyrebski@gmail.com>
2018-07-01 12:35:05 +0200

    Green. AddGoal when one team scores 10 goals, scoring team should win 1st set

--- Foosball.Domain/Game.cs
+++ Foosball.Domain/Game.cs
@@ -41,6 +41,20 @@ namespace Foosball.Domain
     public class Set
     {
         public IList<Goal> Goals { get; set; } = new List<Goal>();
+
+        public Team? WonByTeam
+        {
+            get
+            {
+                int goalsA = Goals.Count(goal => goal.Team == Team.TeamA);
+                if (goalsA == 10)
+                    return Team.TeamA;
+                int goalsB = Goals.Count(goal => goal.Team == Team.TeamB);
+                if (goalsB == 10)
+                    return Team.TeamB;
+                return null;
+            }
+        }
     }
 
     public class Goal
