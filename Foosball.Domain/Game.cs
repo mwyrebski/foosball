f607278224fe573a93db9e276ddbc93cf3bc2096
Michal Wyrebski <michal.wyrebski@gmail.com>
2018-07-01 11:44:55 +0200

    Green. Create should pass and return not null

---
+++ Foosball.Domain/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foosball.Domain
+{
+    public class Game
+    {
+        public static Game Create()
+        {
+            return new Game();
+        }
+    }
+}
