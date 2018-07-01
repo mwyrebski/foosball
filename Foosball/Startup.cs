e149411af2b4d88550a51b1a5271ff010324e0ed
Michal Wyrebski <michal.wyrebski@gmail.com>
2018-07-01 16:08:46 +0200

    Serialize enums as strings in Json response

--- Foosball/Startup.cs
+++ Foosball/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Foosball
 {
@@ -33,7 +35,10 @@ namespace Foosball
 
             services.AddScoped<IGameRepository, GameRepository>();
 
-            services.AddMvc();
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.Converters.Add(new StringEnumConverter());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
